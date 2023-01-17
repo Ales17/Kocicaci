575a5285124cd83a03161f12a68ff36bf65b1aa5
AlesB <alesbacul@gmail.com>
2023-01-17 11:40:55 +0100

    Got rid of PHP and JS experiments

--- test.php
+++
@@ -1,7 +0,0 @@
-<a href=http://www.yourdomain.com/index.php>Home</a><br />
-
-<a href=http://www.yourdomain.com/about.php>About</a><br />
-
-<a href=http://www.yourdomain.com/links.php>Links</a><br />
-
-<a href=http://www.yourdomain.com/contact.php>Contact</a><br />
